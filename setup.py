c84b92777cc5f52b9f679368f0a93810801e29a8
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2018-09-14 15:35:44 +0200

    add required packages in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup
 
-setup(name="freehead",
-      version="0.1",
-      description="Functions for head tracking experiments with Optotrak and Pupil Labs",
-      author="Julius Krumbiegel",
-      license="MIT",
-      packages=["freehead"],
-      zip_safe=False)
+setup(
+    name="freehead",
+    version="0.1",
+    description="Functions for head tracking experiments with Optotrak and Pupil Labs",
+    author="Julius Krumbiegel",
+    license="MIT",
+    packages=["freehead"],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'zmq',
+        'pylsl',
+        'msgpack',
+    ],
+    zip_safe=False)
