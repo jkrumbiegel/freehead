c9963ff3aa7db22e51618fcc523adbaadde18be4
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2018-09-16 15:51:25 +0200

    rename input arguments for clarity, add source

--- freehead/multidim_ortho_procrustes.py
+++ freehead/multidim_ortho_procrustes.py
@@ -1,11 +1,12 @@
 import numpy as np
 
 
-def multidim_ortho_procrustes(As, Bs):
-    # from As to Bs, reference onto measured
-    # As & Bs : N x M x 3
+def multidim_ortho_procrustes(measured, reference):
+    # from reference onto measured
+    # both N x M x 3
+    # from https://www.cse.iitb.ac.in/~ajitvr/CS763_Spring2017/procrustes.pdf
 
-    u, w, vt = np.linalg.svd(np.einsum('nmi,nmj->nij', Bs, As))
+    u, w, vt = np.linalg.svd(np.einsum('nmi,nmj->nij', reference, measured))
     # we flip vt to v and u to ut by using the indices in reverse, which is like transposing before multiplication
     Rs = np.einsum('nji,nkj->nik', vt, u)
     determinants = np.linalg.det(Rs)
