25ada8a4ec51987f20a6ab978961a2c0abe8d60b
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2018-09-24 14:40:04 +0200

    add flipyz option

--- freehead/tup3d.py
+++ freehead/tup3d.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 
-def tup3d(array: np.ndarray):
+def tup3d(array: np.ndarray, flipyz=False):
     assert(array.ndim == 2 and array.shape[1] == 3)
-    return array[:, 0], array[:, 1], array[:, 2]
+    if flipyz:
+        return array[:, 0], array[:, 2], array[:, 1]
+    else:
+        return array[:, 0], array[:, 1], array[:, 2]
