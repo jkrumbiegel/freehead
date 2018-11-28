2648a88dbfaed35860fdffaef18997e00325fb1b
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2018-11-28 11:33:05 +0100

    add option for multiple keys to check in was_key_pressed

--- freehead/was_key_pressed.py
+++ freehead/was_key_pressed.py
@@ -1,9 +1,7 @@
 import pygame
 
 
-def was_key_pressed(key):
-    keydowns = []
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            keydowns.append(event.key)
-    return key in keydowns
+def was_key_pressed(*keys):
+    keydowns = [e.key for e in pygame.event.get() if e.type == pygame.KEYDOWN]
+
+    return tuple(key in keydowns for key in keys) if len(keys) > 1 else keys[0] in keydowns
